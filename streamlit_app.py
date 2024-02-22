d4b3ebdeeff9a0bac51bb4184e1b43c7041dd00e
Cormac <csmith715@gmail.com>
2024-02-22 16:45:03 -0500

    Adjusting for format

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 def main():
-    st.title("Excel File to Pandas DataFrame Converter")
+    st.title("Excel File Converter")
 
     # Create a button for file upload
     uploaded_file = st.file_uploader("Upload Excel file", type=["xls", "xlsx"])
@@ -10,14 +10,14 @@ def main():
     if uploaded_file is not None:
         try:
             # Load the Excel file into a Pandas DataFrame
-            df = pd.read_excel(uploaded_file, sheet_name='Client Summary')
+            df = pd.read_excel(uploaded_file, sheet_name='Client Summary', usecols='C:G', skiprows=6, header=None)
             st.success("File uploaded successfully. DataFrame created.")
-
+            formatted_df = df[df[4] != 0].fillna('')
             # Display the DataFrame
-            st.write(df)
+            st.write(formatted_df)
 
             # Optionally, you can provide a button to download the DataFrame as a CSV
-            st.markdown(get_csv_download_link(df), unsafe_allow_html=True)
+            st.markdown(get_csv_download_link(formatted_df), unsafe_allow_html=True)
 
         except Exception as e:
             st.error(f"Error: {e}")
@@ -27,6 +27,7 @@ def get_csv_download_link(df):
     csv = df.to_csv(index=False)
     href = f'<a href="data:file/csv;base64,{csv}" download="data.csv">Download CSV File</a>'
     return href
+
 
 if __name__ == "__main__":
     main()
